038325ba6a4625ed73845d822f2c9a32cd598def
Zach Schneider <me@zachschneider.com>
2023-05-11 12:44:06 -0500

    Linting

--- tracking.js
+++ tracking.js
@@ -1,19 +1,19 @@
 // Unobtrusive Event Tracking
-$('.track').on('click', function() {
-    // Defined variables for tracking
-    var eCategory = $(this).data("category");
-    var eAction   = $(this).data("action");
-    var eLabel    = $(this).data("label");
+$(".track").on("click", function () {
+  // Defined variables for tracking
+  var eCategory = $(this).data("category");
+  var eAction = $(this).data("action");
+  var eLabel = $(this).data("label");
 
-    // Test
-    // console.log(eCategory + ", " + eAction + ", " + eLabel);
+  // Test
+  // console.log(eCategory + ", " + eAction + ", " + eLabel);
 
-    // Google Event Track
-    ga('send', 'event', eCategory, eAction, eLabel);
-  
-    // Redirect to the href
-    window.open(this.href);
-  
-    // Don't jump the gun
-    return false;
+  // Google Event Track
+  ga("send", "event", eCategory, eAction, eLabel);
+
+  // Redirect to the href
+  window.open(this.href);
+
+  // Don't jump the gun
+  return false;
 });
